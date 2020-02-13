a4f7b8dae2ce34b394a35241a83f82955fe4cac4
Kaivalya Mendki <kaivalya.mendki@thoughtworks.com>
2020-02-13 23:49:07 +0530

    [Kaivalya] #2.3 Add. Equates Customer with same library numbers

--- src/test/java/com/tw/pathashala69/biblioteca/core/models/CustomerTest.java
+++ src/test/java/com/tw/pathashala69/biblioteca/core/models/CustomerTest.java
@@ -3,8 +3,7 @@ package com.tw.pathashala69.biblioteca.core.models;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 
 class CustomerTest {
@@ -16,4 +15,19 @@ class CustomerTest {
         assertThat(customer.libraryNumber(), is(equalTo("123-4567")));
     }
 
+    @Test
+    public void shouldEquateTwoCustomersWithSameLibraryNumber() {
+        Customer customer = new Customer("123-4567", "password");
+        Customer anotherCustomer = new Customer("123-4567", "password");
+
+        assertThat(customer, is(equalTo(anotherCustomer)));
+    }
+
+    @Test
+    public void shouldNotEquateCustomersHavingDifferentLibraryNumber() {
+        Customer customer = new Customer("123-4567", "password");
+        Customer anotherCustomer = new Customer("123-4789", "password1");
+
+        assertThat(customer, is(not(equalTo(anotherCustomer))));
+    }
 }
