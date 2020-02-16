e0ba6862f2fe2466d99aa0362753b0b7fd52c2fe
Kaivalya Mendki <kaivalya.mendki@thoughtworks.com>
2020-02-16 16:02:39 +0530

    [Kaivalya] #2.3 Add. Logout menu item logs out current logged in user

--- src/main/java/com/tw/pathashala69/biblioteca/core/menu/LogoutItem.java
+++ src/main/java/com/tw/pathashala69/biblioteca/core/menu/LogoutItem.java
@@ -1,17 +1,20 @@
 package com.tw.pathashala69.biblioteca.core.menu;
 
+import com.tw.pathashala69.biblioteca.core.auth.UserAuthentication;
 import com.tw.pathashala69.biblioteca.core.ui.AuthInterface;
 
 public class LogoutItem extends BaseMenuItem {
     private final AuthInterface authInterface;
+    private final UserAuthentication userAuth;
 
-    public LogoutItem(String title, String symbol, AuthInterface authInterface) {
+    public LogoutItem(String title, String symbol, AuthInterface authInterface, UserAuthentication userAuth) {
         super(title, symbol);
         this.authInterface = authInterface;
+        this.userAuth = userAuth;
     }
 
     @Override
     public void onSelect() {
-
+        userAuth.logout();
     }
 }
--- src/test/java/com/tw/pathashala69/biblioteca/core/menu/LogoutItemTest.java
+++ src/test/java/com/tw/pathashala69/biblioteca/core/menu/LogoutItemTest.java
@@ -1,19 +1,29 @@
 package com.tw.pathashala69.biblioteca.core.menu;
 
+import com.tw.pathashala69.biblioteca.core.auth.UserAuthentication;
 import com.tw.pathashala69.biblioteca.core.ui.AuthInterface;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class LogoutItemTest {
 
-    private LogoutItem logoutItem;
+    private UserAuthentication userAuth;
     private AuthInterface authInterface;
+    private LogoutItem logoutItem;
 
     @BeforeEach
     void setUp() {
+        userAuth = mock(UserAuthentication.class);
         authInterface = mock(AuthInterface.class);
-        logoutItem = new LogoutItem("Logout", "LO", authInterface);
+        logoutItem = new LogoutItem("Logout", "LO", authInterface, userAuth);
     }
 
+    @Test
+    public void shouldLogoutTheLoggedInUser() {
+        logoutItem.onSelect();
+
+        verify(userAuth, times(1)).logout();
+    }
 }
